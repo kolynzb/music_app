2b2d6d5ec3d0509351df1e3953f04a588babf1c9
kolynzb <collinsbenda360@gmail.com>
2022-08-15 14:02:34 +0300

    Add Color constants

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,5 +1,10 @@
 import 'dart:ui';
 
+import 'package:music_app/utils/hex_to_color.dart';
+
 class AppColors {
-  static const Color name = Color(0xFFccc7c5);
+  static Color orange = hexToColor("#FB5A00");
+  static Color blue = hexToColor("#4b90f7");
+  static Color purple = hexToColor("#674FF");
+  static Color pink = hexToColor("#DB17DF");
 }
