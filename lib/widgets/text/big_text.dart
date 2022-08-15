67ff28ef71fea5a8d59994c094ff7af16e92008f
kolynzb <collinsbenda360@gmail.com>
2022-08-15 17:28:40 +0300

    Add Big text widget

--- lib/widgets/text/big_text.dart
+++ lib/widgets/text/big_text.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:storee/utils/colors.dart';
-import 'package:storee/utils/dimension.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:music_app/constants/dimension.dart';
 
 class BigText extends StatelessWidget {
   Color? color;
   final String text;
   double size;
-  TextOverflow overflow;
+  // TextOverflow overflow;
 
-  BigText(
-      {Key? key,
-      this.color = const Color(0xFF332d2b),
-      required this.text,
-      this.size = 0,
-      this.overflow = TextOverflow.ellipsis})
-      : super(key: key);
+  BigText({
+    Key? key,
+    this.color = const Color(0xFF332d2b),
+    required this.text,
+    this.size = 0,
+    // this.overflow = TextOverflow.ellipsis,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      overflow: overflow,
+      // overflow: overflow,
       maxLines: 1,
-      style: TextStyle(
-        color: color,
-        fontWeight: FontWeight.w400,
-        fontSize: size == 0 ? Dimensions.font20 : size,
+      style: GoogleFonts.nunito(
+        textStyle: TextStyle(
+          color: color,
+          fontWeight: FontWeight.w400,
+          fontSize: size == 0 ? Dimensions.font20 : size,
+        ),
       ),
     );
   }
